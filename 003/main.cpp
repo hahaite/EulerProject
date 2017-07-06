3830f9206d048cc0d9a7b3595105e5b72900c000
LEE HoSang <hahaite@innopiatech.com>
2017-07-06 14:19:54 +0900

    solved - problem 3

---
+++ 003/main.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <sys/time.h>
+
+#include "./../prime/prime.h"
+
+using namespace std ;
+
+int main()
+{
+	timeval tFirst ;
+	timeval tSecond ;
+	timeval tWorking ;
+	gettimeofday(&tFirst, NULL) ;
+	/////////////////////////////////////////////////////////////////////
+
+	CPrime prime ;
+
+	long long value = 600851475143 ;
+
+	long long curPrime ;
+	long long ret ;
+
+	while(1)
+	{
+		curPrime = prime.getNextPrime() ;	
+
+		if( value % curPrime == 0)
+		{
+			value /= curPrime ;
+			prime.resetPrime() ;
+
+			if(prime.isPrime(value))
+				break ;
+		}
+	}
+
+	printf("value : %lld\n", value) ;
+
+
+	/////////////////////////////////////////////////////////////////////
+	gettimeofday(&tSecond, NULL) ;
+	timersub(&tSecond, &tFirst, &tWorking) ;
+	printf("Working Time : [%d.%06d\n", tWorking.tv_sec, tWorking.tv_usec) ;
+	return 1 ;
+}
