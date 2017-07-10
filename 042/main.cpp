460e7c0b0585a3662a7c5c9e742a6d60f93ff22c
LEE HoSang <hahaite@innopiatech.com>
2017-07-10 15:52:07 +0900

    replace space to tab

--- 042/main.cpp
+++ 042/main.cpp
@@ -15,58 +15,58 @@ int main()
 	gettimeofday(&tFirst, NULL) ;
 	/////////////////////////////////////////////////////////////////////
 
-        set<int>                setSum ;
-        set<int>::iterator      setSumIter ;
+	set<int>		setSum ;
+	set<int>::iterator      setSumIter ;
 
-        for(int ii = 1; ii < 50; ii++)
-                setSum.insert(ii * (ii + 1) / 2) ;
+	for(int ii = 1; ii < 50; ii++)
+		setSum.insert(ii * (ii + 1) / 2) ;
 
-        FILE* fp = fopen("words.txt", "rt") ;
-        if(!fp)
-                return 0 ;
+	FILE* fp = fopen("words.txt", "rt") ;
+	if(!fp)
+		return 0 ;
 
-        char szText[32000] ;
+	char szText[32000] ;
 
-        fgets(szText, sizeof(szText), fp) ;
+	fgets(szText, sizeof(szText), fp) ;
 
-        char* pa ;
-        string strA ;
-        string::iterator strIter ;
-        int sum = 0 ;
-        int count = 0 ;
+	char* pa ;
+	string strA ;
+	string::iterator strIter ;
+	int sum = 0 ;
+	int count = 0 ;
 
-        pa = strtok(szText, ",") ;
+	pa = strtok(szText, ",") ;
 
-        while(1)
-        {
-                strA = pa ;
-                strIter = strA.begin() ;
-                strA.erase(strIter) ;
+	while(1)
+	{
+		strA = pa ;
+		strIter = strA.begin() ;
+		strA.erase(strIter) ;
 
-                strIter = strA.end() ;
-                strIter-- ;
-                strA.erase(strIter) ;
+		strIter = strA.end() ;
+		strIter-- ;
+		strA.erase(strIter) ;
 
-                sum = 0 ;
-                strIter = strA.begin() ;
-                for(; strIter != strA.end(); strIter++)
-                        sum += *strIter - 'A' + 1 ;
+		sum = 0 ;
+		strIter = strA.begin() ;
+		for(; strIter != strA.end(); strIter++)
+			sum += *strIter - 'A' + 1 ;
 
-                setSumIter = setSum.find(sum) ;
+		setSumIter = setSum.find(sum) ;
 
-                if(strA == "SKY")
-                        printf("sum : %d\n", sum) ;
+		if(strA == "SKY")
+			printf("sum : %d\n", sum) ;
 
-                if(setSumIter != setSum.end())
-                        count++ ;
+		if(setSumIter != setSum.end())
+			count++ ;
 
-                pa = strtok(NULL, ",") ;
+		pa = strtok(NULL, ",") ;
 
-                if(pa == NULL)
-                        break ;
-        }
-        fclose(fp) ;
-        printf("Result : %d\n", count) ;
+		if(pa == NULL)
+			break ;
+	}
+	fclose(fp) ;
+	printf("Result : %d\n", count) ;
 
 	/////////////////////////////////////////////////////////////////////
 	gettimeofday(&tSecond, NULL) ;
